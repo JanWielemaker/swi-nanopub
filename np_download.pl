ecafc0227e3ef29c107b18837b797595566f8c43
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-12-01 14:10:23 +0100

    Download plain triples

--- np_download.pl
+++ np_download.pl
@@ -1,10 +1,14 @@
 :- module(np_download,
-          [ np_download/1
+          [ np_download/1,
+            np_triples/2,      % Hash, Triples
+            np_graphs/5        % +Triples, NPURI, Assertions, Prov, Pub
           ]).
 :- use_module(library(http/http_open)).
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(semweb/rdf_http_plugin)).
 :- use_module(library(semweb/turtle)).
+
+:- rdf_register_prefix(np, 'http://www.nanopub.org/nschema#').
 
 np_server('http://np.inn.ac/').
 
@@ -15,3 +19,24 @@ np_download(Hash) :-
     np_server(Server),
     atom_concat(Server, Hash, URL),
     rdf_load(URL, [format(trig)]).
+
+%!  np_triples(+Hash, -Triples)
+
+np_triples(Hash, Triples) :-
+    np_server(Server),
+    atom_concat(Server, Hash, URL),
+    setup_call_cleanup(
+        (   http_open(URL, In, []),
+            set_stream(In, encoding(utf8))
+        ),
+        rdf_read_turtle(In, Triples, []),
+        close(In)).
+
+np_graphs(Triples, NPURI,
+          AssertionGraph, ProvGraph, PubGraph) :-
+    rdf_equal(np:hasAssertion, PA),
+    rdf_equal(np:hasProvenance, PPo),
+    rdf_equal(np:hasPublicationInfo, PPi),
+    memberchk(rdf(NPURI, PA,  AssertionGraph, HeadGraph:_), Triples),
+    memberchk(rdf(NPURI, PPo, ProvGraph, HeadGraph:_), Triples),
+    memberchk(rdf(NPURI, PPi,  PubGraph, HeadGraph:_), Triples).
