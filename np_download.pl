f6a282cfff70c365fb0d9ec32905e95f6d424388
Tobias Kuhn <kuhntobias@gmail.com>
2017-12-07 13:07:07 +0100

    Add new 'get_nanopub/2' predicate

--- np_download.pl
+++ np_download.pl
@@ -1,7 +1,8 @@
 :- module(np_download,
           [ np_download/1,
             np_triples/2,      % Hash, Triples
-            np_graphs/5        % +Triples, NPURI, Assertions, Prov, Pub
+            np_graphs/5,       % +Triples, NPURI, Assertions, Prov, Pub
+			get_nanopub/2      % +NanopubID, -Nanopub
           ]).
 :- use_module(library(http/http_open)).
 :- use_module(library(semweb/rdf_db)).
@@ -12,7 +13,7 @@
 
 np_server('http://np.inn.ac/').
 
-hash('RAvJLzqaDFPAVwd9qbwSjXFlZMVuFZBvm6qlGUqZUCPY0').
+% Example artifact code: RAvJLzqaDFPAVwd9qbwSjXFlZMVuFZBvm6qlGUqZUCPY0
 
 
 np_download(Hash) :-
@@ -22,9 +23,9 @@ np_download(Hash) :-
 
 %!  np_triples(+Hash, -Triples)
 
-np_triples(Hash, Triples) :-
+np_triples(ArtifactCode, Triples) :-
     np_server(Server),
-    atom_concat(Server, Hash, URL),
+    atom_concat(Server, ArtifactCode, URL),
     setup_call_cleanup(
         (   http_open(URL, In, []),
             set_stream(In, encoding(utf8))
@@ -40,3 +41,22 @@ np_graphs(Triples, NPURI,
     memberchk(rdf(NPURI, PA,  AssertionGraph, HeadGraph:_), Triples),
     memberchk(rdf(NPURI, PPo, ProvGraph, HeadGraph:_), Triples),
     memberchk(rdf(NPURI, PPi,  PubGraph, HeadGraph:_), Triples).
+
+
+get_nanopub(NanopubID, Nanopub) :-
+	(
+		trustyuri_artifactcode(NanopubID, ArtifactCode)
+	;
+		artifactcode(NanopubID, 'RA'),
+		ArtifactCode = NanopubID
+	),
+	np_triples(ArtifactCode, Nanopub).
+
+trustyuri_artifactcode(TrustyUri, ArtifactCode) :-
+	sub_atom(TrustyUri, _, _, _, '://'),
+	sub_atom(TrustyUri, _, 45, 0, ArtifactCode).
+
+artifactcode(ArtifactCode, Type) :-
+	atom_length(ArtifactCode, 45),
+	sub_atom(ArtifactCode, 0, 2, _, Type).
+
